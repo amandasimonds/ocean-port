a1dfe78e8018fcd988911d964b34f48797926060
hdcarr87 <hdmanley87@gmail.com>
2020-03-10 16:01:07 -0600

    info

--- client/src/components/Learn/ReefFishInfo.js
+++ client/src/components/Learn/ReefFishInfo.js
@@ -8,24 +8,21 @@ const ReefFishInfo = props => {
             <MammalsWrapper>
             <Card>
                 <CardHeader>
-                    <h1>Let's learn about Marine Mammals</h1>
+                    <h1>Let's learn about Coral Reefs and the fish that live there!</h1>
                 </CardHeader>
                 <CardBody>
                     <p1>
-                        <img src={`${process.env.PUBLIC_URL}/assets/images/mammals.jpg`} alt="a picture of all mammals" />
+                        <img src={`${process.env.PUBLIC_URL}/assets/images/reef.jpg`} alt="a picture of a coarl reef" />
                     </p1>
                     <p2>
                         <br></br>
                         <br></br>
                         <br></br>
                         <br></br>
-                    There are five different animals that are in the catagory of sea mammal, they are sea otters, polar bears, cetaceans, pinnipeds, and sirenisans.
-                    Cetaceans are animals like whales, dolphins and porpoises.
-                    Sirenians are animals like manatees and dugongs, also known as sea cows.
-                    Cetaceans consist of over 90 different whales, dolphins, and porpoises: 14 baleen whales, 7 porpoises, 3 sperm whales, 22 beaked whales, 38 dolphins, 4 river dolphins, beluga whales, and narwals.
+                    A coral reef is a collection of living coral that form a community that build on rocks in warm, shallow waters.  The coral connect to each other using calcium carbonite, similar to calcium found in our bones.  They rely on the sun for their energy and food.  You can think of coral reefs as the rainforests of the ocean.  25% or 1/4 of the animals that live in the ocean, live on a coral reef.  Reefs provide a home, not only to coral but also fish, mollusks, worms, crustaceans, echinoderms, sponges, tunicates and other cnidarians.
                     </p2>
                     <p3>
-                    <img src={`${process.env.PUBLIC_URL}/assets/images/bluewhale.jpg`} alt="picture of a bluewhale" />
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/blacktailbutteryfly.jpg`} alt="picture of a blacktailbutterfly fish" />
                     </p3>
                     <p2>
                         <br></br>
@@ -42,11 +39,10 @@ const ReefFishInfo = props => {
                         <br></br>
                         <br></br>
                         <br></br>
-                    Baleen whales eat small animals called plancton and have hair like filters in their mouths instead of teeth. These help the whale catch the small animals they eat and push the rest of the water out of their mouths. The largest marine mammal is a blue whale (a baleen whale), averaging 98 feet long and weighing 200 tons!
-                    Dolphins and porpoises are similar, porpoises just have smaller bodies and a shorter snout. Both species eat small fish and live in groups called pods.
+                    Let's explore the fish that live on the reef!  These beautiful fish are native to the Red Sea and in the Mediterranean off the coast of Israel.  Adults generally live in pairs are patrol a particular territory, while the younger fish live in the coral branches and are not territorial.  They eat coral polyps and sea anemone tentacles.
                     </p2>
                     <p1>
-                    <img src={`${process.env.PUBLIC_URL}/assets/images/Beluga.jpg`} alt="picture of a beluga whale" />
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/maskedbutterflyfish.jpg`} alt="picture of a masked butterfly fish" />
                     </p1>
                     <p2>
                         <br></br>
@@ -61,11 +57,10 @@ const ReefFishInfo = props => {
                         <br></br>
                         <br></br>
                         <br></br>
-                    Beaked whales are whales that live in deep ocean water.  They are able to hold their breath for a very long time and do not have to come to the surface for air very often.  They are called beaked whales because they have a snout similar in shape to a dolphin.  They mostly eat deep water squid, but will also eat fish or crustations.  They catch their food but enlarging their throats and sucking their food into their mouths.
-                    Blugas and narwals live in the cold waters of the Artic ocean.  They are foragers, they will eat fish, shrimp, crustations.  During the winter the whales migrate slightly south to avoid being underniether the ice cap as it forms.
+                    The masked butterfly fish, also known as bluecheek butterfly, is also found in the Red Sea and one of the larger species of butterfly fish.  Masked butterfly fish are one of the few species of fish that mate for life.  They eat hard corals and benthic invertebrates, which are like sea worms.
                     </p2>
                     <p3>
-                    <img src={`${process.env.PUBLIC_URL}/assets/images/manatees.jpg`} alt="picture of a manatees" />
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/blackwedgebutteryfish.jpg`} alt="picture of a blackwedge butterfly fish" />
                     </p3>
                     <p2>
                         <br></br>
@@ -80,10 +75,10 @@ const ReefFishInfo = props => {
                         <br></br>
                         <br></br>
                         <br></br>
-                    Sea cows got their name because they are similar to land cows in that they are large, slow, and live by eating grass.  Sea cows weigh about 3,300 pounds, and live by eating sea grass! They live in warm sallow waters on the coasts.  Sadly, because they are so slow, they are often times run over by boats or caught in fishing nets. Today they are on the endangered species list.
+                    Blackwedge butterfly fish are found mostly in the Idian Ocean, from eastern Africa all the way to Indonesia.  They can grow to be almost 8 inches long.  They are found on the edges of reef in the currents.  They eat mostly invertebrates.
                     </p2>
                     <p1>
-                    <img src={`${process.env.PUBLIC_URL}/assets/images/polarbear.jpg`} alt="picture of a polarbear" />
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/triggerfish.jpg`} alt="picture of a triggerfish" />
                     </p1>
                     <p2>
                         <br></br>
@@ -99,7 +94,7 @@ const ReefFishInfo = props => {
                         <br></br>
                         <br></br>
                         <br></br>
-                    Polar Bears live exclusivly in the Artic, which is the northern most part of the world. Polar Bears can swim for days at a time. They eat mostly seals but will also eat young walrus or whales when they can.  They hunt mainly on floating ice burges.  Polar Bears actually have black skin with thick transparent fur, so we see a white bear!  This black skin and think fur help the bear to soak up the sun and stay warm. Polar bear mothers give birth while in hibernation in November or December in ice caves. The babies are only about the size of a guinea pig! The cubs will nurse while the mother hibernates, and when they come out of the cave in about 5 months the cubs will the size of a dog.
+                    Triggerfish make up about 40 species of brightly colored fish usually with lines and spots. Mostly living in the Indo-Pacific in coastal habitats, and coral reefs. Triggerfish can grow up to 20 inches long. Triggerfish eat shellfish, so they have small strong jaws with teeth shaped for crushing.
                     </p2>
                 </CardBody>
             </Card>
